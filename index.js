ce6814b716dbe1a81b1f1c68c0a130863d2dd30a
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-13 02:16:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+ 
+function happyHolidaysTo(name) {
+  happyHolidaysTo("Jane")
+  return "Happy holidays, ${name}!"
+} 
+
+function happyHolidaysTo(name) {
+  
+  return `Happy ${holiday}, ${name}!`
+} 
+
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo("Jane") // "Hello, Jane!"
+sayHelloTo("R2-D2") // "Hello, R2-D2!"
+sayHelloTo(1) // "Hello, 1!"
