7c338e79ba90c7cb989bcb3017ea1cfd6fe6b4b4
Shayne Paddock <shayne@nerdystuff.ca>
2018-01-13 02:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 } 
 
-function holidayCountdown(holiday, name) {
-  happyHolidaysTo(Thanksgiving, Jane)
-  return `Happy ${holiday}, ${name}!`
+function holidayCountdown(holiday, days) {
+  holidayCountdown(Thanksgiving, 92)
+  return `It's ${days} days until ${holiday}!`
 } 
